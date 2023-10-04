4465bad47acc65eab3a72cdc84863337bfb1bbbb
Luai Bashar <luai.hawa@gmail.com>
2023-10-04 16:15:35 -0400

    Added comments

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 
+# Sets up GUI and size
 box = tk.Tk()
 box.title("Pacemaker GUI")
 box.geometry("500x500")
 box.minsize(400, 300)
 
+# Variables to use for text font
 titleFont = ('Helvatical bold',14)
 subtextFont = ('Helvatical bold',12)
 
+# Start page
 def startPage():
     title = tk.Label(box, text="Welcome to the PULSEMASTER Interface", font=titleFont, bg="lightblue", height=2)
     title.pack(fill=tk.BOTH)
@@ -18,5 +21,6 @@ def startPage():
     registerButton = tk.Button(box, text ="Register", font=subtextFont)
     registerButton.pack(side="top", expand=True)  
 
+# Start GUI with start page
 startPage()
 box.mainloop()
