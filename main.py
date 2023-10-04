6a83ee240cb250f03a7095107918a47b2adb1f2f
Luai Bashar <luai.hawa@gmail.com>
2023-10-04 15:37:54 -0400

    added buttons and starting text

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-import tkinter
+import tkinter as tk
 
-box = tkinter.Tk()
+box = tk.Tk()
 box.title("Pacemaker GUI")
 box.geometry("500x500")
+box.minsize(200, 200)
+
+titleFont = ('Helvatical bold',14)
+subtextFont = ('Helvatical bold',12)
 
 def startPage():
-    button = tkinter.Button(box, text ="Click")
-    button.pack(side="top", expand=True)   
+    title = tk.Label(box, text="Hello, tkinter!", font=titleFont)
+    title.pack()
+
+    loginButton = tk.Button(box, text ="Login", font=subtextFont)
+    loginButton.pack(side="top", expand=True)  
+
+    registerButton = tk.Button(box, text ="Register", font=subtextFont)
+    registerButton.pack(side="top", expand=True)  
 
 startPage()
 box.mainloop()
