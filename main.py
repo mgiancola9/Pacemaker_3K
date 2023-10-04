b7f7137800b314aa9ba84742010a39138f17897b
Luai Bashar <luai.hawa@gmail.com>
2023-10-04 15:12:36 -0400

    set startPage function

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# hello world!
+import tkinter
+
+box = tkinter.Tk()
+box.title("Pacemaker GUI")
+box.geometry("500x500")
+
+def startPage():
+    button = tkinter.Button(box, text ="Click")
+    button.pack(side="top", expand=True)   
+
+startPage()
+box.mainloop()
