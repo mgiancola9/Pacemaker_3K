405e26bbacebfef0b88d33ef1ced99c82fad309c
Luai Bashar <luai.hawa@gmail.com>
2023-10-04 16:44:08 -0400

    set redirect function and login page

--- main.py
+++ main.py
@@ -10,16 +10,38 @@ box.minsize(400, 300)
 titleFont = ('Helvatical bold',14)
 subtextFont = ('Helvatical bold',12)
 
+# Clears the current page, and redirects to the new one
+currentPage = None
+def redirectPage(newPage):
+    global currentPage
+    if currentPage:
+        currentPage.pack_forget()
+
+    currentPage = newPage
+    newPage.pack(fill=tk.BOTH)
+
 # Start page
 def startPage():
-    title = tk.Label(box, text="Welcome to the PULSEMASTER Interface", font=titleFont, bg="lightblue", height=2)
+    startPage = tk.Frame(box)
+    redirectPage(startPage)
+
+    title = tk.Label(startPage, text="Welcome to the PULSEMASTER Interface", font=titleFont, bg="lightblue", height=2)
     title.pack(fill=tk.BOTH)
 
-    loginButton = tk.Button(box, text ="Login", font=subtextFont)
+    loginButton = tk.Button(startPage, text ="Login", font=subtextFont,command=loginPage)
     loginButton.pack(expand=True)  
 
-    registerButton = tk.Button(box, text ="Register", font=subtextFont)
+    registerButton = tk.Button(startPage, text ="Register", font=subtextFont)
     registerButton.pack(side="top", expand=True)  
+
+# Login page
+def loginPage():
+    loginPage = tk.Frame(box)
+    redirectPage(loginPage)
+
+    title = tk.Label(loginPage, text="Yuh", font=titleFont, bg="lightblue", height=2)
+    title.pack(fill=tk.BOTH)
+
 
 # Start GUI with start page
 startPage()
