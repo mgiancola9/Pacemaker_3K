74409ab361d55a983a1464d97799c8a692942ac2
Luai Bashar <luai.hawa@gmail.com>
2023-10-04 16:06:01 -0400

    Added title background colour

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import tkinter as tk
 box = tk.Tk()
 box.title("Pacemaker GUI")
 box.geometry("500x500")
-box.minsize(200, 200)
+box.minsize(400, 300)
 
 titleFont = ('Helvatical bold',14)
 subtextFont = ('Helvatical bold',12)
 
 def startPage():
-    title = tk.Label(box, text="Hello, tkinter!", font=titleFont)
-    title.pack()
+    title = tk.Label(box, text="Welcome to the PULSEMASTER Interface", font=titleFont, bg="yellow", height=2)
+    title.pack(fill=tk.BOTH)
 
     loginButton = tk.Button(box, text ="Login", font=subtextFont)
-    loginButton.pack(side="top", expand=True)  
+    loginButton.pack(expand=True)  
 
     registerButton = tk.Button(box, text ="Register", font=subtextFont)
     registerButton.pack(side="top", expand=True)  
